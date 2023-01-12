247d1dab467f567db34c15be7a0e1ec432e0924f
Anastasia Karachova <57650422+undefined-person@users.noreply.github.com>
2023-01-13 01:03:48 +0200

    fix(title): fix styles for title

--- src/ui/common/Title/Title.tsx
+++ src/ui/common/Title/Title.tsx
@@ -4,8 +4,9 @@ import { TitleStyles } from './Title.styles'
 
 interface TitleProps {
   title: string
+  size: 'small' | 'medium' | 'large'
 }
 
-export const Title: FC<TitleProps> = ({ title }) => {
-  return <TitleStyles>{title}</TitleStyles>
+export const Title: FC<TitleProps> = ({ title, size }) => {
+  return <TitleStyles size={size}>{title}</TitleStyles>
 }
