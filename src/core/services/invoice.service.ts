da9afddf9cdd671153add5785da8a6c8610194a5
Anastasia Karachova <57650422+undefined-person@users.noreply.github.com>
2023-01-15 18:38:10 +0200

    fix(invoiceService):  replace axios instance by baseQuery

--- src/core/services/invoice.service.ts
+++ src/core/services/invoice.service.ts
@@ -1,27 +1,45 @@
-import { api } from 'core/utils'
-import { IInvoiceList, IInvoicesQuery, InvoiceInfo } from 'core/models'
 import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react'
 
-const baseQuery = (config: any) => {
-  return api(config)
-}
+import { ACCESS_TOKEN } from 'core/constants'
+import { IInvoiceList, IInvoicesQuery, InvoiceDto, InvoiceInfo } from 'core/models'
 
 export const invoiceApi = createApi({
   reducerPath: 'invoiceApi',
-  baseQuery,
+  baseQuery: fetchBaseQuery({
+    baseUrl: import.meta.env.VITE_API_URL,
+    prepareHeaders: (headers) => {
+      const token = localStorage.getItem(ACCESS_TOKEN)
+      if (token) {
+        headers.set('Authorization', `Bearer ${token}`)
+      }
+      return headers
+    },
+  }),
+  tagTypes: ['Invoice'],
   endpoints: (builder) => ({
     getInvoices: builder.query<IInvoiceList, IInvoicesQuery>({
       query: ({ status, limit, offset }) => ({ url: '/invoice', params: { status, limit, offset } }),
+      providesTags: ['Invoice'],
     }),
     getInvoice: builder.query<InvoiceInfo, string>({
       query: (id: string) => `/invoice/${id}`,
+      providesTags: ['Invoice'],
     }),
     createInvoice: builder.mutation({
-      query: (body: any) => ({
+      query: (body: InvoiceDto) => ({
         url: '/invoice',
         method: 'POST',
         body,
       }),
+      invalidatesTags: ['Invoice'],
+    }),
+    draftInvoice: builder.mutation({
+      query: (body: InvoiceDto) => ({
+        url: '/invoice/draft',
+        method: 'POST',
+        body,
+      }),
+      invalidatesTags: ['Invoice'],
     }),
     updateInvoice: builder.mutation({
       query: (body: any) => ({
@@ -29,12 +47,21 @@ export const invoiceApi = createApi({
         method: 'PUT',
         body,
       }),
+      invalidatesTags: ['Invoice'],
     }),
     deleteInvoice: builder.mutation({
-      query: (id: string) => ({
+      query: (id: number) => ({
         url: `/invoice/${id}`,
         method: 'DELETE',
       }),
+      invalidatesTags: ['Invoice'],
+    }),
+    markInvoiceAsPaid: builder.mutation({
+      query: (id: number) => ({
+        url: `invoice/paid/${id}`,
+        method: 'PATCH',
+      }),
+      invalidatesTags: ['Invoice'],
     }),
   }),
 })
