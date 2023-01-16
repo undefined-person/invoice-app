adc38c84b77322f730ded701eb5e3c34376633b8
Anastasia Karachova <57650422+undefined-person@users.noreply.github.com>
2023-01-16 14:35:17 +0200

    fix(ClientAddress): add defaultValues for ClientAddress

--- src/ui/components/CreateModal/components/ClientAddress/ClientAddress.tsx
+++ src/ui/components/CreateModal/components/ClientAddress/ClientAddress.tsx
@@ -1,7 +1,9 @@
 import { FC } from 'react'
 import { FieldValues, UseFormRegister } from 'react-hook-form'
 
+import { IClientAddress } from 'core/models'
 import { FormInput } from 'ui/common'
+
 import { AddressContainer } from './ClientAddress.styles'
 import { CreateModalGroup } from '../../CreateModal.styles'
 
@@ -9,16 +11,45 @@ interface ClientAddressProps {
   register: UseFormRegister<FieldValues>
   errors: FieldValues
   type: 'client' | 'sender'
+  defaultValues?: IClientAddress
 }
 
-export const ClientAddress: FC<ClientAddressProps> = ({ errors, register, type }) => {
+export const ClientAddress: FC<ClientAddressProps> = ({ errors, register, type, defaultValues }) => {
   return (
     <CreateModalGroup>
-      <FormInput type="text" name={`${type}StreetAddress`} label="Street Address" register={register} errors={errors} />
+      <FormInput
+        defaultValue={defaultValues?.street}
+        type="text"
+        name={`${type}StreetAddress`}
+        label="Street Address"
+        register={register}
+        errors={errors}
+      />
       <AddressContainer>
-        <FormInput type="text" name={`${type}City`} label="City" register={register} errors={errors} />
-        <FormInput type="text" name={`${type}PostCode`} label="Post Code" register={register} errors={errors} />
-        <FormInput type="text" name={`${type}Country`} label="Country" register={register} errors={errors} />
+        <FormInput
+          defaultValue={defaultValues?.city}
+          type="text"
+          name={`${type}City`}
+          label="City"
+          register={register}
+          errors={errors}
+        />
+        <FormInput
+          defaultValue={defaultValues?.postCode}
+          type="text"
+          name={`${type}PostCode`}
+          label="Post Code"
+          register={register}
+          errors={errors}
+        />
+        <FormInput
+          defaultValue={defaultValues?.country}
+          type="text"
+          name={`${type}Country`}
+          label="Country"
+          register={register}
+          errors={errors}
+        />
       </AddressContainer>
     </CreateModalGroup>
   )
